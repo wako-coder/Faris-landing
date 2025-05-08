48332f22435a6cf5d3c47de6c5523cc7544c2cb3
Abdi <abdistar16@gmail.com>
2025-05-08 11:52:49 +0300

    location update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -37,7 +37,7 @@
                                 </div>
                                 <div class="contact-info-content">
                                     <h5>Call Us</h5>
-                                    <p><a href="tel:+251902122222">+2 513 654 7898</a></p>
+                                    <p><a href="tel:+251902122222">+251-902122222</a></p>
                                 </div>
                             </div>
                             <div class="contact-info">
@@ -103,7 +103,7 @@
 
     <!-- Google Map Embed Section -->
      <div class="contact-map">
-        <iframe src="https://maps.google.com/maps?q=Gurd%20Shola,%20Addis%20Ababa,%20Ethiopia&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" style="width: 100%; height: 400px; border:0;" allowfullscreen loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.6986789194925!2d38.767658340064585!3d8.999849189480514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8531e27112ed%3A0x445f642620603061!2sSur%20construction%20Head%20office!5e0!3m2!1sen!2set!4v1746693336245!5m2!1sen!2set" frameborder="0" style="width: 100%; height: 400px; border:0;" allowfullscreen loading="lazy"></iframe>
     </div>
 
 </main>
