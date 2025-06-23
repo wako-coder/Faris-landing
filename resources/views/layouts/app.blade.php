a54670d127381bee88e9f498f71eba3dc201941d
wakoya kumsa <waqo1234567@gmail.com>
2025-06-23 11:40:00 +0300

    referrer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content>
     <meta name="keywords" content>
+<meta name="referrer" content="strict-origin-when-cross-origin">
 
     <title>FARIS Technology – Better Technology, Better Business.</title>
 
